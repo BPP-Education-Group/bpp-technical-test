3e7bccbcc02df82776116be625ae5171aefb880a
Alex Hunter <AlexHunter@bpp.com>
2023-06-19 00:02:24 +0100

    adding swe test, and removing legacy

--- software-engineer-3/types/RegistrationForm.ts
+++
@@ -1,6 +0,0 @@
-export type RegistrationForm = {
-  firstName: string
-  lastName: string
-  email: string
-  courseId: string
-}
--- software-engineer/backend/src/routes/courses.js
+++
@@ -1,9 +0,0 @@
-// routes/courses.js
-const express = require('express');
-const router = express.Router();
-const coursesController = require('../controllers/courses');
-
-router.get('/', coursesController.getAllCourses);
-router.get('/:id', coursesController.getCourseById);
-
-module.exports = router;
