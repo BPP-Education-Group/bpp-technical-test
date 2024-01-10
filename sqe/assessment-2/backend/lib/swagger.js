f7351789cd7decbe9bb69e2a591cdd2cec654ee2
jdrydn <james@jdrydn.com>
2024-01-10 16:51:50 +0000

    fix: Swagger documentation

--- sqe/assessment-2/backend/lib/swagger.js
+++ sqe/assessment-2/backend/lib/swagger.js
@@ -3,7 +3,16 @@ const swaggerJsDoc = require('swagger-jsdoc');
 
 const swaggerSpec = swaggerJsDoc({
   swaggerDefinition: {
-
+    info: {
+      openapi: '3.0.3',
+      title: 'Todo App API documentation',
+      version: '1.0.0',
+    },
+    servers: [{ url: 'http://localhost:3000/api' }],
+    tags: [
+      { name: 'Lists' },
+      { name: 'Tasks' },
+    ],
   },
   apis: [
     path.resolve(__dirname, '../routes/*.js'),
